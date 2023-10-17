07caf441a064ac8e892f1ec538b828b2057c96d2
Pieter Samyn <github@samyne.be>
2023-10-17 22:15:42 +0200

    prepare start version

---
+++ src/MyDigitalDiary.WebApi7/Controllers/DiaryController.cs
@@ -0,0 +1,26 @@
+﻿using DigitalDiary;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyDigitalDiary.WebApi7.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class DiaryController : ControllerBase
+{
+    private readonly IDigitalDiaryRepository _repository;
+
+    public DiaryController(IDigitalDiaryRepository repository)
+    {
+        _repository = repository;
+    }
+
+    [HttpGet]
+    public Task<IActionResult> GetRepositoryProjectName() => Task.FromResult((IActionResult)new OkObjectResult(_repository.RepositoryProjectName));
+
+    [HttpPost]
+    public Task<IActionResult> Post([FromBody] string message)
+    {
+        _repository.Write(message);
+        return Task.FromResult((IActionResult)new OkResult());
+    }
+}
